a8f80504137be8045233c00aeda03b8cad644460
PierricM380 <pierric.marye@wanadoo.fr>
2021-08-30 18:01:20 +0200

    Animated header title

---
+++ app.js
@@ -0,0 +1,22 @@
+var headerAnim1 = document.querySelector('h1');
+
+var typewriter = new Typewriter(headerAnim1, {
+	deleteSpeed: 50
+});
+
+typewriter
+	.changeDelay(50)
+	.typeString("Moi c'est <strong> Pierric Marye,</strong> ")
+	.pauseFor(500)
+	.typeString('<strong> développeur web et web mobile</strong>')
+	.pauseFor(1000)
+	.deleteChars(17)
+    .pauseFor(1000)
+	.typeString('<strong>Front-End</strong>')
+    .pauseFor(1000)
+	.deleteChars(9)
+	.typeString('<strong>Back-End</strong>')
+    .pauseFor(1000)
+	.deleteChars(8)
+	.typeString('<strong>iOS</strong>')
+	.start();
