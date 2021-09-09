cbe6a41294c384864cf6a3d5099feaec1d95f65c
PierricM380 <pierric.marye@wanadoo.fr>
2021-09-09 13:47:06 +0200

    Modifications delays for typewriter animJS adding properties Scroll event for nav

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ typewriter
 	.typeString('<strong>Développeur web et mobile</strong>')
 	.pauseFor(1000)
 	.deleteChars(13)
-    .pauseFor(500)
+	.pauseFor(500)
 	.typeString('<strong><span style="color: #ff7f50">Front-End</span></strong>')
-    .pauseFor(1000)
+	.pauseFor(1000)
 	.deleteChars(9)
 	.pauseFor(500)
 	.typeString('<strong><span style="color: #07617d">Back-End</span></strong>')
-    .pauseFor(1000)
+	.pauseFor(1000)
 	.deleteChars(8)
 	.pauseFor(500)
 	.typeString('<strong><span style="color: #ff6347">iOS</span></strong>')
@@ -28,17 +28,17 @@ typewriter
 
 // animJS animation
 AOS.init({
-	easing:'linear',
-	duration: 1000
+	easing: 'linear',
+	duration: 1000,
 })
 
 // Scroll Event Navigation bar
 const nav = document.querySelector('nav')
 
 window.addEventListener('scroll', () => {
-			if (window.scrollY < 120) {
-					nav.style.top = '0'
-				} else {
-					nav.style.top = '-130px'
-				}
-			})
+	if (window.scrollY < 120) {
+		nav.style.top = '0'
+	} else {
+		nav.style.top = '-140px'
+	}
+});
